262dded9d914afca9321591380093de700ca3f25
michael coker <mcoker@gmail.com>
2017-10-31 11:49:23 -0400

    wrap paypal form in container

--- content/Store.php
+++ content/Store.php
@@ -1,37 +1,44 @@
 <h1>Store</h1>
 
 <div class="divider">
-    <p>Buy some items from my store. If you have any questions, please
+    <p>Gift giving made easy! Just click the buy now button for gift certificates and hand crafted oils at your fingertips.
+        If you have any questions, please
         <a href="/?go=Contact">contact me</a>.</p>
 </div>
 
 <div class="item">
     <h2>Gift Certificate</h2>
     <p>
-        Gift certificate description...
+        Gift certificates are offered in 1hr, 1.5hr, and 2hr time sessions. This way you don't have to choose what type of session
+        your special someone gets! Simply click buy now. If you want me to send a shiny gift certificate directly to the
+        recipient, just provide me with their name, address and who you would like me to say it is from. You are also welcome
+        to craft your own card, note or email directly to them and just have them tell me who originally purchased it so
+        I can keep track of my transactions.
     </p>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypal">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="3T3G3LCLGNAWW">
-        <table>
-            <tr>
-                <td>
-                    <input type="hidden" name="on0" value="Session Length">Session Length</td>
-            </tr>
-            <tr>
-                <td>
-                    <select name="os0">
-                        <option value="1hr Session">1hr Session $105.00 USD</option>
-                        <option value="1.5hr Session">1.5hr Session $150.00 USD</option>
-                        <option value="2hr Session">2hr Session $200.00 USD</option>
-                    </select>
-                </td>
-            </tr>
-        </table>
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-    </form>
+    <div class="paypal">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="3T3G3LCLGNAWW">
+            <table>
+                <tr>
+                    <td>
+                        <input type="hidden" name="on0" value="Session Length">Session Length</td>
+                </tr>
+                <tr>
+                    <td>
+                        <select name="os0">
+                            <option value="1hr Session">1hr Session $105.00 USD</option>
+                            <option value="1.5hr Session">1.5hr Session $150.00 USD</option>
+                            <option value="2hr Session">2hr Session $200.00 USD</option>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+    </div>
 </div>
 
 <div class="item">
@@ -43,12 +50,14 @@
         <em>Nine sacred Maya herbs collected with prayer from the mineral rich rainforest of Belize, Sacred Earth Organic Massage
             Oil Blend, Essential oils of Ylang Ylang, Orange, Lavender, and Lemon Grass.</em>
     </p>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypal">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="28MZEBEGCCMJY">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-    </form>
+    <div class="paypal">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="28MZEBEGCCMJY">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+    </div>
 </div>
 
 <div class="item">
@@ -60,10 +69,21 @@
     <p>Ingredients:
         <em>Copal resin sustainably harvested from the rainforest in Belize and Sacred Earth Organic Massage Oil Blend.</em>
     </p>
-    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" class="paypal">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="DZCX7N6FB2CNG">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-    </form>
+    <div class="paypal">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_xclick">
+            <input type="hidden" name="business" value="charlotteshealinghands@gmail.com">
+            <input type="hidden" name="lc" value="US">
+            <input type="hidden" name="item_name" value="Copal Oil">
+            <input type="hidden" name="amount" value="15.00">
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="button_subtype" value="services">
+            <input type="hidden" name="no_note" value="0">
+            <input type="hidden" name="tax_rate" value="8.250">
+            <input type="hidden" name="shipping" value="6.00">
+            <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+    </div>
 </div>
